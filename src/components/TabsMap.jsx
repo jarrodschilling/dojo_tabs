eb5b4718bb28bd773a69a4da0a8697d63b425b2d
jarrodschilling <jarrodschilling@gmail.com>
2024-01-16 13:49:19 -0700

    submitting

--- src/components/TabsMap.jsx
+++ src/components/TabsMap.jsx
@@ -1,16 +1,30 @@
 import { useState } from "react";
 
 const TabsMap = props => {
-    const tabList = [
+    // const [tabList, setTabList] = useState([])
+    // const [tabName, setTabName] = useState("")
+    // const [tabContent, setTabContent] = useState("")
+    // const [tabMarker, setTabMarker] = useState(false)
+    const [showAll, setShowAll] = useState("")
+    const newTabList = [
         {tabName: "Web Fund", tabContent: "Web Fund Stuff"},
         {tabName: "Python", tabContent: "python_stuff"},
         {tabName: "javascript", tabContent: "javaScriptStuff"},
         {tabName: "JAVA", tabContent: "I'm super STRICT!!"},
     ]
-    const [showAll, setShowAll] = useState("")
+    
+    // const runMarker = randomList => {
+    //     const newList = randomList.map(obj => {
+    //         obj.tabMarker = tabMarker
+    //         return obj
+    //     })
+    //     setTabList(newList)
+    // }
+    // runMarker(newTabList)
+    
     
     const handleClick = chkIdx => {
-        const displayTab = tabList.filter((obj, idx) => idx === chkIdx)
+        const displayTab = newTabList.filter((obj, idx) => idx === chkIdx)
         
         
         // add map function to highlight the clicked tab
@@ -25,7 +39,7 @@ const TabsMap = props => {
         <>
         <div className="jsApp">
             {
-                tabList.map((tab, index) => (
+                newTabList.map((tab, index) => (
                     <div className="nameRow" key={index}>
                         <button className="jsBtn" onClick={ (e) => handleClick(index)}>{tab.tabName}</button>
                     </div>
